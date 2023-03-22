4c392a71aa9dc89151fc73d19667a8ac5229c067
hachan140 <havu14102@gmail.com>
2023-03-23 00:08:04 +0700

    upload project

---
+++ Project_PRN211/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_PRN211.Models
+{
+    public partial class Employee
+    {
+        public short Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateBirth { get; set; }
+        public int? Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public decimal? Salary { get; set; }
+        public override string ToString()
+        {
+            return $"{Username}";
+        }
+    }
+}
